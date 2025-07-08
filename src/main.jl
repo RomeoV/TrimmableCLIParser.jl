bf905f1d3b19cc7d59a8367d95abea27b161d8c4
Romeo Valentin <romeov@stanford.edu>
2025-07-08 17:41:04 +0200

    Slightly refactor main file.

--- src/main.jl
+++ src/main.jl
@@ -12,30 +12,32 @@ const EXAMPLE_ARGS = ["--verbose", "--port", "1234"]
 function main(args = ARGS)
     cli_schema = EXAMPLE_CLI_SCHEMA
     config = parse_args(cli_schema, args)
-
-    if config.verbose
-        println("Verbose mode is on!")
-    end
-    println("Processing with port: ", config.port, " and rate: ", config.rate)
-    println(Core.stdout, config)
+    print_config(config)
     return nothing # Return Nothing for a clean JET report
 end
 
 Base.@ccallable function main(argc::Cint, argv::Ptr{Ptr{Cchar}})::Cint
     cli_schema = EXAMPLE_CLI_SCHEMA
     config = parse_args(cli_schema, argc, argv)
-
-    if config.verbose
-        println(Core.stdout, "Verbose mode is on!")
-    end
-    println(Core.stdout, "Processing with port $(config.port)")
-    println(Core.stdout, "and rate $(config.rate)")
+    print_config(config)
     return 0
 end
 Base.Experimental.entrypoint(main, (Cint, Ptr{Ptr{Cchar}}))
 
+@inline function print_config(config)
+    if config.verbose
+        println(Core.stdout, "Verbose mode is on!")
+    end
+    println(Core.stdout, "[Experiment $(config.name)]")
+    print(Core.stdout, "Processing with port $(config.port) ")
+    println(Core.stdout, "and rate $(config.rate).")
+    return nothing
+end
+
 # --- JET.jl Analysis example ---
 # @report_opt TrimmableCLIParser.parse_args(TrimmableCLIParser.EXAMPLE_CLI_SCHEMA, TrimmableCLIParser.EXAMPLE_ARGS)
+# or
+# @report_opt main()
 
 if abspath(PROGRAM_FILE) == @__FILE__
     main(ARGS)
