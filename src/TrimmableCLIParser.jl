d59d86e1d05fab14d646489e8e640c4f6a73048e
Romeo Valentin <romeov@stanford.edu>
2025-07-08 15:32:19 +0200

    Attempt rewrite to `SumTypes.jl`

--- src/TrimmableCLIParser.jl
+++ src/TrimmableCLIParser.jl
@@ -5,46 +5,70 @@ using SumTypes
 
 public parse_args, ArgSpec
 
+@sum_type ArgVal begin
+    IntVal(x::Int)
+    FloatVal(x::Float64)
+    StringVal(s::String)
+    BoolVal(b::Bool)
+end
+getvalue(x::ArgVal) = @cases x begin
+    [IntVal, FloatVal, StringVal, BoolVal](x) => x
+end
+
 @sum_type ArgSpec begin
     Flag(long::String, short::String, help::String)
-    StringOption(long::String, short::String, help::String, type::Type{String}, default::String)
-    IntOption(long::String, short::String, help::String, type::Type{Int}, default::Int)
-    FloatOption(long::String, short::String, help::String, type::Type{Float64}, default::Float64)
+    StringOption(long::String, short::String, help::String, default::String)
+    IntOption(long::String, short::String, help::String, default::Int)
+    FloatOption(long::String, short::String, help::String, default::Float64)
+end
+getlong(spec::ArgSpec) = @cases spec begin
+    [StringOption, IntOption, FloatOption](long, _, _, _) => long
+    Flag(long, _, _) => long
+end
+getshort(spec::ArgSpec) = @cases spec begin
+    [StringOption, IntOption, FloatOption](_, short, _, _) => short
+    Flag(_, short, _) => short
+end
+gettype(spec::ArgSpec) = @cases spec begin
+    StringOption => String
+    IntOption => Int
+    FloatOption => Float64
+    Flag => Bool
 end
 
 maybeparse(T::Type{<:Number}, x) = parse(T, x)
 maybeparse(T::Type{<:AbstractString}, x) = string(x)
 
 function parse_one_spec(spec::ArgSpec, args::Vector{String})
-    idx = findfirst(a -> a == spec.long || a == spec.short, args)
+    idx = findfirst(∈((getlong(spec), getshort(spec))), args)
     return @cases spec begin
-        Flag(_, _, _) => !isnothing(idx)
-        IntOption(_, _, _, T, default) => begin
-            isnothing(idx) && return default
+        Flag(_, _, _) => BoolVal(!isnothing(idx))
+        IntOption(_, _, _, default) => begin
+            isnothing(idx) && return IntVal(default)
             #
             if (idx + 1) > length(args)
                 error("Argument $(spec.long) requires a value.")
             end
             val_str = args[idx + 1]
-            return maybeparse(T, val_str)
+            return IntVal(parse(Int, val_str))
         end
-        FloatOption(_, _, _, T, default) => begin
-            isnothing(idx) && return default
+        FloatOption(_, _, _, default) => begin
+            isnothing(idx) && return FloatVal(default)
             #
             if (idx + 1) > length(args)
                 error("Argument $(spec.long) requires a value.")
             end
             val_str = args[idx + 1]
-            return maybeparse(T, val_str)
+            return FloatVal(parse(Float64, val_str))
         end
-        StringOption(_, _, _, T, default) => begin
-            isnothing(idx) && return default
+        StringOption(_, _, _, default) => begin
+            isnothing(idx) && return StringVal(default)
             #
             if (idx + 1) > length(args)
                 error("Argument $(spec.long) requires a value.")
             end
             val_str = args[idx + 1]
-            return maybeparse(T, val_str)
+            return StringVal(string(val_str))
         end
     end
 end
@@ -54,19 +78,21 @@ function parse_args(schema::NTuple{N, ArgSpec} where {N}, argc::Cint, argv::Ptr{
     args = unsafe_string.(
         [unsafe_load(argv, i) for i in 1:argc]
     )
-    parse_args(schema, args)
+    return parse_args(schema, args)
 end
 
 function parse_args(schema::NTuple{N, ArgSpec} where {N}, args::Vector{String} = ARGS)
-    keys = map(s -> Symbol(replace(s.long, "--" => "")), schema)
+    keys = map(s -> Symbol(replace(getlong(s), "--" => "")), schema)
+    # types = map(gettype, schema)
     values = map(s -> parse_one_spec(s, args), schema)
     return NamedTuple{keys}(values)
+    # return NamedTuple{keys, Tuple{types...}}(map(getvalue, values))
 end
 
 EXAMPLE_CLI_SCHEMA = (
     Flag("--verbose", "-v", "Enable verbose logging"),
-    IntOption("--port", "-p", "The port to listen on", Int, 8080),
-    FloatOption("--rate", "-r", "The processing rate", Float64, 1.5),
+    IntOption("--port", "-p", "The port to listen on", 8080),
+    FloatOption("--rate", "-r", "The processing rate", 1.5),
 )
 EXAMPLE_ARGS = ["--verbose", "--port", "1234"]
 
@@ -100,7 +126,7 @@ Base.@ccallable function main(argc::Cint, argv::Ptr{Ptr{Cchar}})::Cint
     println(Core.stdout, "Processing with port: ", config.port, " and rate: ", config.rate)
     println(Core.stdout, config)
     foreach(config) do c
-        println(Core.stdout, c);
+        println(Core.stdout, c)
         nothing
     end
     return 0
