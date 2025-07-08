9fa97179f2e2cddc107a2b6ea5b339bc04c6779d
Romeo Valentin <romeov@stanford.edu>
2025-07-08 17:10:10 +0200

    Add some docstrings.

--- src/TrimmableCLIParser.jl
+++ src/TrimmableCLIParser.jl
@@ -7,7 +7,6 @@ import Moshi.Match: @match
 export parse_args, ArgSpec, OptionValWithDefault
 
 @data OptionValWithDefault begin
-    # default values
     IntVal(Int)
     FloatVal(Float64)
     StringVal(String)
@@ -19,6 +18,7 @@ getdefault(val::OptionValWithDefault.Type) = @match val begin
 end
 gettype(val::OptionValWithDefault.Type) = typeof(getdefault(val))
 
+"One `ArgSpec` defines a single flag."
 @data ArgSpec begin
     struct Flag
         long::String
@@ -33,33 +33,43 @@ gettype(val::OptionValWithDefault.Type) = typeof(getdefault(val))
     end
 end
 
+# don't parse strings
 maybeparse(T::Type{<:Number}, x) = parse(T, x)
 maybeparse(T::Type{<:AbstractString}, x) = string(x)
 
+"Search through the vector of `args` for a single `spec` and process it."
 function parse_one_spec(spec::ArgSpec.Type, args::Vector{String})
     idx = findfirst(a -> a == spec.long || a == spec.short, args)
     return @match spec begin
         ArgSpec.Flag(_, _, _) => !isnothing(idx)
         ArgSpec.Option(_, _, _, optionval) => begin
             isnothing(idx) && return getdefault(optionval)
-            #
-            if (idx + 1) > length(args)
-                error("Argument $(spec.long) requires a value.")
-            end
+            (idx + 1) > length(args) && error("Argument $(spec.long) requires a value.")
             val_str = args[idx + 1]
             return maybeparse(gettype(optionval), val_str)
         end
     end
 end
 
-function parse_args(schema::NTuple{N, ArgSpec.Type} where {N}, argc::Cint, argv::Ptr{Ptr{Cchar}})
-    args = unsafe_string.(
-        [unsafe_load(argv, i) for i in 1:argc]
-    )
+"""
+    parse_args(schema, argc, argv)
+
+C-style interface, taking in `argc::Cint` and `argv::Ptr{Ptr{Cchar}}`.
+Will crash if `argc > length(args)`.
+"""
+function parse_args(schema::NTuple{N, ArgSpec.Type} where {N},
+                    argc::Cint, argv::Ptr{Ptr{Cchar}})
+    args = [unsafe_string(unsafe_load(argv, i)) for i in 1:argc]
     return parse_args(schema, args)
 end
 
-function parse_args(schema::NTuple{N, ArgSpec.Type} where {N}, args::Vector{String} = ARGS)
+"""
+    parse_args(schema, args)
+
+Main entry point. Statically infers key names and value types, and returns a named tuple.
+"""
+function parse_args(schema::NTuple{N, ArgSpec.Type} where {N},
+                    args::Vector{<:AbstractString} = ARGS)
     keys = map(s -> Symbol(replace(s.long, "--" => "")), schema)
     values = map(s -> parse_one_spec(s, args), schema)
     return NamedTuple{keys}(values)
